e49184690bec1fd0fa3d3465a14640848fb91753
BrandonDG <brandon.gillespie08@gmail.com>
2019-02-08 11:37:42 -0800

    Dummy client now sends messages in the decided message format. The nodejs server parses the message and creates JSON object out of it. Validation is next

--- server/apw_server.js
+++ server/apw_server.js
@@ -3,10 +3,13 @@ const net = require('net')
 //console.log("Hello I am Nodejs")
 net.createServer(socket => {
   socket.on('data', function(data){
-    buf1 = Buffer.alloc(80)
+    buf1 = Buffer.alloc(1024)
     buf1.write(data.toString())
     console.log('Echoing: %s', data.toString())
     socket.write(buf1)
+
+    var message = JSON.parse(data.toString())
+    console.log(message.type)
   })
   socket.on('error', (err) => {
     //console.log("Error Found")
